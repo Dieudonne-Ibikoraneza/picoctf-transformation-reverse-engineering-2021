d1d831c57df271dee7210c1d2121221c4e38de43
IBIKORANEZA Dieudonne <dieudonneibikoraneza13@gmail.com>
2025-02-10 09:20:22 +0200

    first commit

---
+++ transformation_reverse_engineering.py
@@ -0,0 +1,11 @@
+enc_flag = "灩捯䍔䙻ㄶ形楴獟楮獴㌴摟潦弸彥ㄴㅡて㝽"
+
+decoded_flag = []
+for c in enc_flag:
+    num = ord(c)
+    first = chr(num >> 8)
+    second = chr(num & 0xFF)
+    decoded_flag.append(first + second)
+
+flag = ''.join(decoded_flag)
+print(flag)
